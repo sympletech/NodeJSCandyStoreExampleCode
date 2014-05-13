2a0c7e6da559a40ac9dd088c8621af71e43a5e41
Daniel Lewis <dewis@sympletech.com>
2014-05-12 18:10:12 -0700

    created home viewmodel

--- CandyStore/public/app/view-models/home-viewmodel.js
+++ CandyStore/public/app/view-models/home-viewmodel.js
@@ -1,7 +1,43 @@
-﻿var homeViewModel = function () {
+﻿var fakeData = [
+    {id : 1, name : 'candy name', price:0.5},
+    {id : 2, name : 'candy name', price:0.5},
+    {id : 3, name : 'candy name', price:0.5},
+    {id : 4, name : 'candy name', price:0.5}
+];
+
+var homeViewModel = function () {
     var self = this;
 
-    self.welcomeMessage = ko.observable("W3lcome To My Spa St@rter");
+    //***************************************
+    //  Candy Catalog
+    //***************************************
+    self.candyChoices = ko.observableArray();
+    
+    self.loadCandyChoices = function() {
+        self.candyChoices(fakeData);
+    };
+    self.loadCandyChoices();
+
+    //***************************************
+    //  Shopping Cart
+    //***************************************
+    self.cart = ko.observableArray();
+
+    self.addCandyToCart = function(candy) {
+        self.cart.push(candy);
+    };
+
+    self.removeCandyFromCart = function(candy) {
+        var smallerList = _.reject(self.cart(), function(cartItem) {
+            return cartItem == candy;
+        });
+        self.cart(smallerList);
+    };
+
+    self.showCheckout = ko.observable(false);
+    self.checkout = function() {
+        self.showCheckout(true);
+    };
 
     return self;
 };
