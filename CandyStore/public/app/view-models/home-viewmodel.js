4ea55d64d64da568e29149d7756d7b9f44b73286
Daniel Lewis <dewis@sympletech.com>
2014-05-12 18:26:37 -0700

    bound home screen up to viewmodel

--- CandyStore/public/app/view-models/home-viewmodel.js
+++ CandyStore/public/app/view-models/home-viewmodel.js
@@ -1,8 +1,10 @@
 ﻿var fakeData = [
-    {id : 1, name : 'candy name', price:0.5},
-    {id : 2, name : 'candy name', price:0.5},
-    {id : 3, name : 'candy name', price:0.5},
-    {id : 4, name : 'candy name', price:0.5}
+    {id : 1, name : 'Green Taffy', price:'0.50'},
+    {id : 2, name : 'Sour Balls', price:'0.75'},
+    {id : 3, name : 'Pixie Sticks', price:'1.50'},
+    {id : 4, name : 'Pez', price:'3.50'},
+    {id : 5, name : 'Black Licorice', price:'0.25'}
+
 ];
 
 var homeViewModel = function () {
@@ -33,6 +35,20 @@ var homeViewModel = function () {
         });
         self.cart(smallerList);
     };
+
+    self.cartTotal = ko.computed(function() {
+        if (self.cart().length == 0) {
+            return 0;
+        }
+
+        var cartPrices = _.map(self.cart(), function(cartItem) {
+            return parseFloat(cartItem.price);
+        });
+        var cartTotal = _.reduce(cartPrices, function(memo, num) {
+            return memo + num;
+        });
+        return cartTotal;
+    });
 
     self.showCheckout = ko.observable(false);
     self.checkout = function() {
