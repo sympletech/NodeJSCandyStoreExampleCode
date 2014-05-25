4b80560739dfe49eedf9737df6a805ba4667c142
dlewis <dlewis@sympletech.com>
2014-05-25 10:24:35 -0700

    got grunt working

---
+++ CandyStore/public/javascripts/JavaScript1.js
@@ -0,0 +1,34 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            sass: {
+                files: ['../CandyStore/public/stylesheets/source/**/*.scss'],
+                tasks: ['compass']
+            }
+        },
+
+        compass: {
+            sass: {
+                options: {
+                    sassDir: '../CandyStore/public/stylesheets/source/',
+                    cssDir: '../CandyStore/public/stylesheets/',
+                    //environment: 'production',
+                    //outputStyle: 'compressed'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    //grunt.loadNpmTasks('grunt-exec');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+};
--- grunt-tasks/Gruntfile.js
+++ grunt-tasks/Gruntfile.js
@@ -1,34 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		
-		watch:{
-			sass:{
-				files:['../CandyStore/public/stylesheets/source/**/*.scss'],
-				tasks: ['compass']
-			}
-		},
-		
-		compass: {
-		  sass: {
-			options: {
-				sassDir: '../CandyStore/public/stylesheets/source/',
-				cssDir: '../CandyStore/public/stylesheets/',
-				//environment: 'production',
-				//outputStyle: 'compressed'
-			}
-		  }
-		}
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-	});
-  
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	//grunt.loadNpmTasks('grunt-exec');
-	//grunt.loadNpmTasks('grunt-contrib-copy');
+        watch: {
+            sass: {
+                files: ['../CandyStore/public/stylesheets/source/**/*.scss'],
+                tasks: ['compass']
+            }
+        },
 
-	// Default task(s).
-	grunt.registerTask('default', ['watch']);
+        compass: {
+            sass: {
+                options: {
+                    sassDir: '../CandyStore/public/stylesheets/source/',
+                    cssDir: '../CandyStore/public/stylesheets/',
+                    //environment: 'production',
+                    //outputStyle: 'compressed'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    //grunt.loadNpmTasks('grunt-exec');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
 };
